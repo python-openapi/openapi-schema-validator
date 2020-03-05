c96183c7763a3054e66212a46a0a4569e02c96ec
p1c2u <maciag.artur@gmail.com>
2020-03-05 11:14:23 +0000

    Datetime format dependencies fix

--- tests/integration/test_validators.py
+++ tests/integration/test_validators.py
@@ -1,7 +1,9 @@
 from jsonschema import ValidationError
+import mock
 import pytest
+from six import u
 
-from openapi_schema_validator import OAS30Validator
+from openapi_schema_validator import OAS30Validator, oas30_format_checker
 
 
 class TestOAS30ValidatorValidate(object):
@@ -24,6 +26,48 @@ class TestOAS30ValidatorValidate(object):
         schema = {"type": schema_type, "nullable": True}
         validator = OAS30Validator(schema)
         value = None
+
+        result = validator.validate(value)
+
+        assert result is None
+
+    @pytest.mark.parametrize('value', [
+        u('1989-01-02T00:00:00Z'),
+        u('2018-01-02T23:59:59Z'),
+    ])
+    @mock.patch(
+        'openapi_schema_validator._format.'
+        'DATETIME_HAS_STRICT_RFC3339', True
+    )
+    @mock.patch(
+        'openapi_schema_validator._format.'
+        'DATETIME_HAS_ISODATE', False
+    )
+    def test_string_format_datetime_strict_rfc3339(self, value):
+        schema = {"type": 'string', "format": 'date-time'}
+        validator = OAS30Validator(
+            schema, format_checker=oas30_format_checker)
+
+        result = validator.validate(value)
+
+        assert result is None
+
+    @pytest.mark.parametrize('value', [
+        u('1989-01-02T00:00:00Z'),
+        u('2018-01-02T23:59:59Z'),
+    ])
+    @mock.patch(
+        'openapi_schema_validator._format.'
+        'DATETIME_HAS_STRICT_RFC3339', False
+    )
+    @mock.patch(
+        'openapi_schema_validator._format.'
+        'DATETIME_HAS_ISODATE', True
+    )
+    def test_string_format_datetime_isodate(self, value):
+        schema = {"type": 'string', "format": 'date-time'}
+        validator = OAS30Validator(
+            schema, format_checker=oas30_format_checker)
 
         result = validator.validate(value)
 
