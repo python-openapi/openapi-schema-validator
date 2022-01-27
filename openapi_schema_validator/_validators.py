3eeb7718657a36ba04a1281c8d849db8ae7c722a
A <maciag.artur@gmail.com>
2022-01-27 04:28:20 +0000

    Merge pull request #30 from gberaudo/support_oneof_discriminator
    Validate oneOf, anyOf and allOf with discriminator

--- openapi_schema_validator/_validators.py
+++ openapi_schema_validator/_validators.py
@@ -1,5 +1,71 @@
 from jsonschema._utils import find_additional_properties, extras_msg
+from jsonschema._validators import oneOf as _oneOf, anyOf as _anyOf, allOf as _allOf
+
 from jsonschema.exceptions import ValidationError, FormatError
+
+
+def handle_discriminator(validator, _, instance, schema):
+    """
+    Handle presence of discriminator in anyOf, oneOf and allOf.
+    The behaviour is the same in all 3 cases because at most 1 schema will match.
+    """
+    discriminator = schema['discriminator']
+    prop_name = discriminator['propertyName']
+    prop_value = instance.get(prop_name)
+    if not prop_value:
+        # instance is missing $propertyName
+        yield ValidationError(
+            "%r does not contain discriminating property %r" % (instance, prop_name),
+            context=[],
+        )
+        return
+
+    # Use explicit mapping if available, otherwise try implicit value
+    ref = discriminator.get('mapping', {}).get(prop_value) or f'#/components/schemas/{prop_value}'
+
+    if not isinstance(ref, str):
+        # this is a schema error
+        yield ValidationError(
+            "%r mapped value for %r should be a string, was %r" % (
+                instance, prop_value, ref),
+            context=[],
+        )
+        return
+
+    try:
+        validator.resolver.resolve(ref)
+    except:
+        yield ValidationError(
+            "%r reference %r could not be resolved" % (
+                instance, ref),
+            context=[],
+        )
+        return
+
+    yield from validator.descend(instance, {
+        "$ref": ref
+    })
+
+
+def anyOf(validator, anyOf, instance, schema):
+    if 'discriminator' not in schema:
+        yield from _anyOf(validator, anyOf, instance, schema)
+    else:
+        yield from handle_discriminator(validator, anyOf, instance, schema)
+
+
+def oneOf(validator, oneOf, instance, schema):
+    if 'discriminator' not in schema:
+        yield from _oneOf(validator, oneOf, instance, schema)
+    else:
+        yield from handle_discriminator(validator, oneOf, instance, schema)
+
+
+def allOf(validator, allOf, instance, schema):
+    if 'discriminator' not in schema:
+        yield from _allOf(validator, allOf, instance, schema)
+    else:
+        yield from handle_discriminator(validator, allOf, instance, schema)
 
 
 def type(validator, data_type, instance, schema):
