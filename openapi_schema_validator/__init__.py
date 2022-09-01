22b12e71a5e244c934114efb020b6d0eb0dc7cee
p1c2u <maciag.artur@gmail.com>
2022-09-01 13:48:17 +0100

    Mypy static type check

--- openapi_schema_validator/__init__.py
+++ openapi_schema_validator/__init__.py
@@ -1,19 +1,19 @@
-# -*- coding: utf-8 -*-
-from openapi_schema_validator._format import oas30_format_checker, \
-    oas31_format_checker
+from openapi_schema_validator._format import oas30_format_checker
+from openapi_schema_validator._format import oas31_format_checker
 from openapi_schema_validator.shortcuts import validate
-from openapi_schema_validator.validators import OAS30Validator, OAS31Validator
+from openapi_schema_validator.validators import OAS30Validator
+from openapi_schema_validator.validators import OAS31Validator
 
-__author__ = 'Artur Maciag'
-__email__ = 'maciag.artur@gmail.com'
-__version__ = '0.3.2'
-__url__ = 'https://github.com/p1c2u/openapi-schema-validator'
-__license__ = '3-clause BSD License'
+__author__ = "Artur Maciag"
+__email__ = "maciag.artur@gmail.com"
+__version__ = "0.3.2"
+__url__ = "https://github.com/p1c2u/openapi-schema-validator"
+__license__ = "3-clause BSD License"
 
 __all__ = [
-    'validate',
-    'OAS30Validator',
-    'oas30_format_checker',
-    'OAS31Validator',
-    'oas31_format_checker',
+    "validate",
+    "OAS30Validator",
+    "oas30_format_checker",
+    "OAS31Validator",
+    "oas31_format_checker",
 ]
--- openapi_schema_validator/validators.py
+++ openapi_schema_validator/validators.py
@@ -1,8 +1,13 @@
-from attr import attrib, attrs
-from copy import deepcopy
+from typing import Any
+from typing import Type
 
-from jsonschema import _legacy_validators, _utils, _validators
-from jsonschema.validators import create, Draft202012Validator, extend
+from jsonschema import _legacy_validators
+from jsonschema import _utils
+from jsonschema import _validators
+from jsonschema.protocols import Validator
+from jsonschema.validators import Draft202012Validator
+from jsonschema.validators import create
+from jsonschema.validators import extend
 
 from openapi_schema_validator import _types as oas_types
 from openapi_schema_validator import _validators as oas_validators
@@ -11,49 +16,49 @@ from openapi_schema_validator._types import oas31_type_checker
 OAS30Validator = create(
     meta_schema=_utils.load_schema("draft4"),
     validators={
-        u"multipleOf": _validators.multipleOf,
+        "multipleOf": _validators.multipleOf,
         # exclusiveMaximum supported inside maximum_draft3_draft4
-        u"maximum": _legacy_validators.maximum_draft3_draft4,
+        "maximum": _legacy_validators.maximum_draft3_draft4,
         # exclusiveMinimum supported inside minimum_draft3_draft4
-        u"minimum": _legacy_validators.minimum_draft3_draft4,
-        u"maxLength": _validators.maxLength,
-        u"minLength": _validators.minLength,
-        u"pattern": _validators.pattern,
-        u"maxItems": _validators.maxItems,
-        u"minItems": _validators.minItems,
-        u"uniqueItems": _validators.uniqueItems,
-        u"maxProperties": _validators.maxProperties,
-        u"minProperties": _validators.minProperties,
-        u"enum": _validators.enum,
+        "minimum": _legacy_validators.minimum_draft3_draft4,
+        "maxLength": _validators.maxLength,
+        "minLength": _validators.minLength,
+        "pattern": _validators.pattern,
+        "maxItems": _validators.maxItems,
+        "minItems": _validators.minItems,
+        "uniqueItems": _validators.uniqueItems,
+        "maxProperties": _validators.maxProperties,
+        "minProperties": _validators.minProperties,
+        "enum": _validators.enum,
         # adjusted to OAS
-        u"type": oas_validators.type,
-        u"allOf": oas_validators.allOf,
-        u"oneOf": oas_validators.oneOf,
-        u"anyOf": oas_validators.anyOf,
-        u"not": _validators.not_,
-        u"items": oas_validators.items,
-        u"properties": _validators.properties,
-        u"required": oas_validators.required,
-        u"additionalProperties": oas_validators.additionalProperties,
+        "type": oas_validators.type,
+        "allOf": oas_validators.allOf,
+        "oneOf": oas_validators.oneOf,
+        "anyOf": oas_validators.anyOf,
+        "not": _validators.not_,
+        "items": oas_validators.items,
+        "properties": _validators.properties,
+        "required": oas_validators.required,
+        "additionalProperties": oas_validators.additionalProperties,
         # TODO: adjust description
-        u"format": oas_validators.format,
+        "format": oas_validators.format,
         # TODO: adjust default
-        u"$ref": _validators.ref,
+        "$ref": _validators.ref,
         # fixed OAS fields
-        u"nullable": oas_validators.nullable,
-        u"discriminator": oas_validators.not_implemented,
-        u"readOnly": oas_validators.readOnly,
-        u"writeOnly": oas_validators.writeOnly,
-        u"xml": oas_validators.not_implemented,
-        u"externalDocs": oas_validators.not_implemented,
-        u"example": oas_validators.not_implemented,
-        u"deprecated": oas_validators.not_implemented,
+        "nullable": oas_validators.nullable,
+        "discriminator": oas_validators.not_implemented,
+        "readOnly": oas_validators.readOnly,
+        "writeOnly": oas_validators.writeOnly,
+        "xml": oas_validators.not_implemented,
+        "externalDocs": oas_validators.not_implemented,
+        "example": oas_validators.not_implemented,
+        "deprecated": oas_validators.not_implemented,
     },
     type_checker=oas_types.oas30_type_checker,
     # NOTE: version causes conflict with global jsonschema validator
     # See https://github.com/p1c2u/openapi-schema-validator/pull/12
     # version="oas30",
-    id_of=lambda schema: schema.get(u"id", ""),
+    id_of=lambda schema: schema.get("id", ""),
     applicable_validators=oas_validators.include_nullable_validator,
 )
 
@@ -61,29 +66,29 @@ OAS31Validator = extend(
     Draft202012Validator,
     {
         # adjusted to OAS
-        u"allOf": oas_validators.allOf,
-        u"oneOf": oas_validators.oneOf,
-        u"anyOf": oas_validators.anyOf,
-        u"description": oas_validators.not_implemented,
-        u"format": oas_validators.format,
+        "allOf": oas_validators.allOf,
+        "oneOf": oas_validators.oneOf,
+        "anyOf": oas_validators.anyOf,
+        "description": oas_validators.not_implemented,
+        "format": oas_validators.format,
         # fixed OAS fields
-        u"discriminator": oas_validators.not_implemented,
-        u"xml": oas_validators.not_implemented,
-        u"externalDocs": oas_validators.not_implemented,
-        u"example": oas_validators.not_implemented,
+        "discriminator": oas_validators.not_implemented,
+        "xml": oas_validators.not_implemented,
+        "externalDocs": oas_validators.not_implemented,
+        "example": oas_validators.not_implemented,
     },
     type_checker=oas31_type_checker,
 )
 
 
-def _patch_validator_with_read_write_context(cls):
+def _patch_validator_with_read_write_context(cls: Type[Validator]) -> None:
     """Adds read/write context to jsonschema validator class"""
     # subclassing validator classes is not intended to
     # be part of their public API and will raise error
     # See https://github.com/p1c2u/openapi-schema-validator/issues/48
     original_init = cls.__init__
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self: Validator, *args: Any, **kwargs: Any) -> None:
         self.read = kwargs.pop("read", None)
         self.write = kwargs.pop("write", None)
         original_init(self, *args, **kwargs)
