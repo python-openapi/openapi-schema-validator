45aa702500fd04746b78f14b0237502b3b6b2c77
p1c2u <maciag.artur@gmail.com>
2023-02-18 03:33:36 +0000

    Formats raise error for other types fix

--- openapi_schema_validator/_format.py
+++ openapi_schema_validator/_format.py
@@ -1,6 +1,7 @@
 import binascii
 from base64 import b64decode
 from base64 import b64encode
+from numbers import Number
 from typing import Any
 from typing import Union
 
@@ -8,41 +9,63 @@ from jsonschema._format import FormatChecker
 
 
 def is_int32(instance: Any) -> bool:
-    return isinstance(instance, int)
+    # bool inherits from int, so ensure bools aren't reported as ints
+    if isinstance(instance, bool):
+        return True
+    if not isinstance(instance, int):
+        return True
+    return ~(1 << 31) < instance < 1 << 31
 
 
 def is_int64(instance: Any) -> bool:
-    return isinstance(instance, int)
+    # bool inherits from int, so ensure bools aren't reported as ints
+    if isinstance(instance, bool):
+        return True
+    if not isinstance(instance, int):
+        return True
+    return ~(1 << 63) < instance < 1 << 63
 
 
 def is_float(instance: Any) -> bool:
+    # bool inherits from int
+    if isinstance(instance, int):
+        return True
+    if not isinstance(instance, Number):
+        return True
     return isinstance(instance, float)
 
 
 def is_double(instance: Any) -> bool:
+    # bool inherits from int
+    if isinstance(instance, int):
+        return True
+    if not isinstance(instance, Number):
+        return True
     # float has double precision in Python
     # It's double in CPython and Jython
     return isinstance(instance, float)
 
 
 def is_binary(instance: Any) -> bool:
-    return isinstance(instance, bytes)
+    if not isinstance(instance, (str, bytes)):
+        return True
+    if isinstance(instance, str):
+        return False
+    return True
 
 
 def is_byte(instance: Union[str, bytes]) -> bool:
+    if not isinstance(instance, (str, bytes)):
+        return True
     if isinstance(instance, str):
         instance = instance.encode()
-    if not isinstance(instance, bytes):
-        return False
 
     encoded = b64encode(b64decode(instance))
     return encoded == instance
 
 
 def is_password(instance: Any) -> bool:
-    if not isinstance(instance, (bytes, str)):
-        return False
-
+    # A hint to UIs to obscure input
     return True
 
 
