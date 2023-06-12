5bf8b10416cded4c2b7fc3b19d854c36036011fb
p1c2u <maciag.artur@gmail.com>
2023-06-12 11:23:10 +0100

    jsonschema 4.18 compatibility

--- openapi_schema_validator/validators.py
+++ openapi_schema_validator/validators.py
@@ -3,12 +3,11 @@ from typing import Any
 from typing import Type
 
 from jsonschema import _legacy_validators
-from jsonschema import _utils
 from jsonschema import _validators
-from jsonschema.protocols import Validator
 from jsonschema.validators import Draft202012Validator
 from jsonschema.validators import create
 from jsonschema.validators import extend
+from jsonschema_specifications import REGISTRY as SPECIFICATIONS
 
 from openapi_schema_validator import _format as oas_format
 from openapi_schema_validator import _types as oas_types
@@ -16,7 +15,9 @@ from openapi_schema_validator import _validators as oas_validators
 from openapi_schema_validator._types import oas31_type_checker
 
 OAS30Validator = create(
-    meta_schema=_utils.load_schema("draft4"),
+    meta_schema=SPECIFICATIONS.contents(
+        "http://json-schema.org/draft-04/schema#",
+    ),
     validators={
         "multipleOf": _validators.multipleOf,
         # exclusiveMaximum supported inside maximum_draft3_draft4
