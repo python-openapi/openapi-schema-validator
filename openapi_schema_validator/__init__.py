07baba285373431268c8cf58833884eaf520a82c
p1c2u <maciag.artur@gmail.com>
2023-10-05 11:15:41 +0000

    Version 0.6.2

--- openapi_schema_validator/__init__.py
+++ openapi_schema_validator/__init__.py
@@ -8,7 +8,7 @@ from openapi_schema_validator.validators import OAS31Validator
 
 __author__ = "Artur Maciag"
 __email__ = "maciag.artur@gmail.com"
-__version__ = "0.6.1"
+__version__ = "0.6.2"
 __url__ = "https://github.com/python-openapi/openapi-schema-validator"
 __license__ = "3-clause BSD License"
 
